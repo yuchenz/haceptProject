8f670cf6217fa7dc252ad916674da1bd95b3393e
Yuchen Zhang <yuchenz@brandeis.edu>
2016-04-18 14:30:37 -0400

    changed subtree_alignment evaluation to be against gold suba

---
+++ subtree_alignment/eval.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python2.7
+
+def eval(gsubaFile, autoFile):
+	gsubaList = [line.split() for line in open(gsubaFile).readlines()]
+	tmp = [line.split() for line in open(autoFile).readlines()]
+	tmp = [line[0].split('--') + line[1:] for line in tmp]
+	tmp = [(line[0][2:], line[1]) for line in tmp if float(line[-1]) <= float(line[-3])]
+	autoSubaList = []
+	sentID = -1
+	for line in tmp:
+		if line[0] != str(sentID):
+			sentID += 1
+			autoSubaList.append([])
+		autoSubaList[-1].append(line[1])
+
+	assert len(autoSubaList) == len(gsubaList), "len autoSubaList == %d, len gsubaList == %d" % (len(autoSubaList), len(gsubaList))
+
+	goldTrue = 0
+	autoTrue = 0
+	correctTrue = 0
+	pairList = zip(gsubaList, autoSubaList)
+	for pair in pairList:
+		goldTrue += len(pair[0])
+		autoTrue += len(pair[1])
+		correctTrue += len(list(set(pair[0]).intersection(set(pair[1]))))
+	
+	print "goldTrue == %d\nautoTrue == %d\ncorrectTrue == %d\n\n" % (goldTrue, autoTrue, correctTrue)
+	p = correctTrue * 1.0 / autoTrue
+	r = correctTrue * 1.0 / goldTrue
+	f = 2 * p * r / (p + r)
+	print "p == %.2f\nr == %.2f\nf == %.2f\n" % (p, r, f)
+
+if __name__ == "__main__":
+	import sys
+	eval(sys.argv[1], sys.argv[2])
--- subtree_alignment/makeFeatFile_fromCompiled.py
+++ subtree_alignment/makeFeatFile_fromCompiled.py
@@ -23,14 +23,16 @@ def makeData(srcF, tgtF, srcTF, tgtTF, waF, gsubaF):
 
 	trainExamples = []
 	f = codecs.open('koala.suba', 'w', 'utf-8')
+	sentID = 0
 	for bead in beadList:
 		for suba in bead.otherSuba:
-			trainExamples.append((features(bead, suba), False))   # add negative training examples
+			trainExamples.append((features(bead, suba), False, str(sentID) + '--' + suba.__str__()))   # add negative training examples
 			f.write(suba.__str__()+' ')
 		for suba in bead.goldSuba:
-			trainExamples.append((features(bead, suba), True))    # add positive training examples
+			trainExamples.append((features(bead, suba), True, str(sentID) + '--' + suba.__str__()))    # add positive training examples
 			f.write(suba.__str__()+' ')
 		f.write('\n')
+		sentID += 1
 	
 	return trainExamples
 
@@ -42,5 +44,5 @@ if __name__ == "__main__":
 	outF = sys.argv[7]
 	f = codecs.open(outF, 'w', 'utf-8')
 	for i, example in enumerate(exampleList):
-		f.write('ID' + str(i) + '\t' +str(example[1]) + '\t' + '\t'.join(example[0]) + '\n')
+		f.write('ID' + example[2] + '\t' +str(example[1]) + '\t' + '\t'.join(example[0]) + '\n')
 	f.close()
