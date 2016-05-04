161988269f82be7589dac041c2eb4b153b571a77
Yuchen Zhang <yuchenz@brandeis.edu>
2016-05-04 10:56:18 -0400

    increased time and memory efficiency of src_wa_pruning/malletAuto2wa.py

--- src_wa_pruning/malletAuto2wa.py
+++ src_wa_pruning/malletAuto2wa.py
@@ -3,6 +3,21 @@
 import sys
 
 def convert(filename, outFilename):
+	sentID = 0
+	result = [[]]
+	for line in open(filename):
+		line = line.split()
+		if float(line[-3]) <= float(line[-1]): continue
+		line = line[0].split('--')
+		line = (int(line[0][2:]), line[1])
+
+		if line[0] != sentID:
+			result.append([])
+			sentID += 1
+		if line[0] == sentID:
+			result[-1].append(line[1])
+
+	'''
 	lines = [line.split() for line in open(filename).readlines()]
 	lines = [line[0].split('--') for line in lines if float(line[-3]) > float(line[-1])] 
 	lines = [[int(line[0][2:]), line[1]] for line in lines]
@@ -15,7 +30,8 @@ def convert(filename, outFilename):
 			sentID += 1
 		if line[0] == sentID:
 			result[-1].append(line[1])
-	
+	'''
+
 	result = [' '.join(line) for line in result]
 	outf = open(outFilename, 'w')
 	for line in result:
